cc9fcaf5b9c32df56533b1a426545b532b9521ca
Max <wijnbaldh.max@gmail.com>
2019-02-28 17:36:43 +0100

    pretty cool

--- amp_connector.py
+++ amp_connector.py
@@ -17,7 +17,7 @@ import ConfigParser
 #parameters:
 
 tenants = {} #dict where key is client id and value is API key [api_key, age_limit, groups]
-sleep_time = 5
+sleep_time = 10
 
 
 def get_json_endpoints(auth_string):
@@ -41,15 +41,9 @@ def main():
 		print('\nAMP4E Endpoint Cleaner')
 		print("______________________________________\n")
 		print("Getting endpoints")
-		#spinner = spinning_cursor()
-		#for _ in range(5):
-		#		sys.stdout.write(next(spinner))
-		#		sys.stdout.flush()
-		#		time.sleep(0.5)
-		#		sys.stdout.write("\a")
 
 		get_tenants()
-		print("Tenants:" + str(tenants))
+		#print("Tenants:" + str(tenants))
 
 
 		for tenant in tenants:
@@ -63,29 +57,44 @@ def main():
 
 			json_data = get_json_endpoints(auth_string)
 			endpoints = get_endpoints(json_data)
-			print("Finding inactive entries")
+			print("Processing inactive entries")
 
 			processed_ep = clean_ep(endpoints, age_limit, group)
 			#print(processed_ep)
 			log_endpoints(processed_ep,tenant)
-			print(action)
+			#print(action)
 			if (action == "delete"):
 				#delete_endpoints(old, auth_string)
 				pass
 
 			elif (action == "log"):
 				log_endpoints(old, client_id)
+			print("Job finished")
 
-			print(processed_ep)
+			#print(processed_ep)
 
-			print("Endpoints processed: " + str(processed_ep))
+			#print("Endpoints processed: " + str(processed_ep))
 
-		print("\nWaiting for next scan")
+		print("\nWaiting for next scan \n")
 
+		spinner = spinning_cursor()
+		current_time = datetime.now()
+		sleep_start = current_time
 
+		while (int((current_time - sleep_start).total_seconds()) < sleep_time):
 
+			for _ in range(9):
+					sys.stdout.write(next(spinner))
+					sys.stdout.flush()
+					time.sleep(0.5)
+					sys.stdout.write("\a")
 
-		time.sleep(sleep_time)
+			
+			sys.stdout.write("\r         ")
+			sys.stdout.write("\r")
+			
+			#sys.stdout.flush()
+			current_time = datetime.now()
 
 
 def get_tenants():
@@ -94,22 +103,17 @@ def get_tenants():
 	config = ConfigParser.ConfigParser()
 	config.read(config_path)
 	global tenants 
-	i = 1
 
-	try:
-		while (i < 5):
-			print(i)
-			client_id = config.get("Tenant " + str(i), "clientID")
-			api_key = config.get("Tenant " + str(i), "apiKey")
-			age_limit = config.get("Tenant " + str(i), "age_limit")
-			group = config.get("Tenant " + str(i), "group")
-			action = config.get("Tenant " + str(i), "action")
-			tenants[client_id] = [api_key, age_limit, group, action] 
+	for section in config.sections():
 
-			i = i + 1
-	except Exception as e:
-		print(e)
-		print("end of config file all good")
+		client_id = config.get(str(section), "clientID")
+		api_key = config.get(str(section), "apiKey")
+		age_limit = config.get(str(section), "age_limit")
+		group = config.get(str(section), "group")
+		action = config.get(str(section), "action")
+		tenants[client_id] = [api_key, int(age_limit), group, action] 
+
+		#	i = i + 1
 
 
 
@@ -160,7 +164,7 @@ def init_params():
 
 def spinning_cursor(): #Cisco Loading bar
 	while True:
-		for cursor in '.:|:.':
+		for cursor in '.:|:.:|:.':
 			yield cursor
 
 def authenticate(client_id, api_key): #Authenticate with AMP4E API Credentials
@@ -195,14 +199,15 @@ def clean_ep(endpoints, age_limit, groups):
 
 	old = []
 	current_time = datetime.now()
-	inactive_time = None
+	inactive_days = None
 
 	for ep in endpoints:
 		guid = endpoints[ep]
 		last_seen = datetime.strptime(guid[0], '%Y-%m-%dT%H:%M:%SZ')
-		print(last_seen)
+		#print(last_seen)
 		group = guid[1]
-		inactive_days = (current_time - last_seen).total_seconds()
+		inactive_days = (current_time - last_seen).days
+		#print(inactive_days)
 
 		#if (inactive_days > age_limit and group in groups):
 		if (inactive_days > age_limit):
@@ -217,13 +222,13 @@ def clean_ep(endpoints, age_limit, groups):
 
 def log_endpoints(endpoints, tenant):
 
-	f= open("expired_endpoints_"+ tenant +".txt","w+")
+	f= open("logs/expired_endpoints_" + tenant + "_" + str(datetime.now()) + ".txt","w+")
 
 	for ep in endpoints:
 		f.write("Endpoint: " + ep + "\n")
 
 	f.close() 
-	print("Wrote to file for tenant " + tenant)  
+	#print("Wrote to file for tenant " + tenant)  
 
 
 #RETURN NONE
@@ -231,7 +236,7 @@ def delete_endpoints(endpoints, auth_string):
 
 	for guid in endpoints:
 		url = auth_string + "/v1/computers/" + guid
-		print guid
+		#print guid
 		response =  requests.request("DELETE", url)
 
 if __name__ == '__main__':
